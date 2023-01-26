9c50f418f4a8877e81d040db0bba67f1a246a649
DataManan <jainmananjain9@gmail.com>
2023-01-26 14:09:43 +0530

    second commit

---
+++ week1/hanoi.py
@@ -0,0 +1,4 @@
+''' ########################################################
+this challenge is to implement a stack data structure to solve problem -> tower of hanoi.
+Here we have three tower (say), A|B|C 
+############################################################'''
