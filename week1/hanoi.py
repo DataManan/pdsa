38e4f4dc94b79265690ee6037f4aba737882656e
DataManan <jainmananjain9@gmail.com>
2023-01-27 13:57:39 +0530

    using oop

--- week1/hanoi.py
+++ week1/hanoi.py
@@ -5,29 +5,6 @@ Here we have three tower (say), A|B|C. Tower A contains 3 rings, we need to tran
 # import stack class
 from stack import Stack
 # implementing stack data
-A = Stack()
-B = Stack()
-C = Stack()
-
-A.push("c")
-A.push("b")
-A.push("a")
-
-print(A.peek())
-A.printstack()
-print(A.stackSize())
-
-"""##############################################################
-solving for tower of hanoi
-#################################################################"""
-
-B.push(A.pop())
-B.push(A.pop())
-B.printstack()
-A.printstack()
-C.push(A.pop())
-C.push(B.pop())
-C.push(B.pop())
-B.printstack()
-A.printstack()
-C.printstack()
+class Hanoi:
+    def __init__(self):
+        self.newStack = Stack()
